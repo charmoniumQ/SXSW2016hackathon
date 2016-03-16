245ffeb08410087bd561777560edc6abc0d5d41f
Caroline Shi <detectiveconan123456789@gmail.com>
2016-03-16 13:25:53 -0500

    Fixed stuff

--- main/client/accounts/performerhome.js
+++ main/client/accounts/performerhome.js
@@ -9,18 +9,21 @@ if (Meteor.isClient) {
     		return Meteor.user().emails[0].address;
   	},
   	gigsA: function(){
-    	return Bids.find({artist: Meteor.userId(),accepted: true}, {sort: {price: -1}});
+    	return Bids.find({artist_id: Meteor.userId(),accepted: true}, {sort: {price: -1}});
     },
     gigsB: function(){
-    	return Bids.find({artist: Meteor.userId(),accepted: false}, {sort: {price: -1}});
+    	return Bids.find({artist_id: Meteor.userId(),accepted: false}, {sort: {price: -1}});
     }
   });
 
   Template.PerformerHome.events({
-    'click #acceptVenues > tbody': function(event) {
+    'click #acceptVenues': function(event) {
       event.preventDefault();
-      var hiddenBidId = $(event.target).closest('tr').data().hiddenID;
+      var hiddenBidId = $(event.target).closest('tr').data().hiddenid;
+      console.log($(event.target).closest('tr').data());
+      console.log(hiddenBidId);
       Bids.update({_id: hiddenBidId},{$set: {accepted: true}});
+      console.log(Bids.find({artist_id: Meteor.userId(),accepted: false}, {sort: {price: -1}}).fetch());
     }
   });
 }
